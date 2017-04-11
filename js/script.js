7bf25caa7752511445f4174be614a6a402e150f4
Mai <maifujii619@gmail.com>
2017-04-11 10:37:57 -0700

    Adds script to count, add and average numbers in array

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var numArr = [];
+
+// Adds input numbers to an array
+function addToArr() {
+  var number = document.getElementById("newnumber").value;
+  numArr.push(number);
+  console.log(JSON.stringify(numArr));  
+}
+  
+function calculate() {
+  // Gets the length of the array for "count"
+  var count = numArr.length;
+  console.log("Count: " + count);
+  
+  
+  // Calculates the sum of the numbers in the array  
+  var sum = 0;
+  for (var i = 0; i < numArr.length; i++){
+    sum += parseFloat(numArr[i]);
+    console.log("Sum: " + sum);  
+  }
+  
+  
+  // Calculates the average of the numbers in the array
+  var avg = sum / numArr.length;
+  console.log("Average: " + avg);
+  
+  
+  // Prints the results in the div
+  document.getElementById("result1").innerHTML = "Count: " + count;
+  document.getElementById("result2").innerHTML = "Sum: " + sum.toFixed(2);
+  document.getElementById("result3").innerHTML = "Average: " + avg.toFixed(2);
+  event.preventDefault(); 
+}
+
+
+
+  
+
